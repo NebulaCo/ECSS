4c1cd4819fef9d98353405cf84837139fed0c813
Syphinx <Kheri1577@gmail.com>
2022-04-11 16:40:14 -0400

    Buttons Images, Session Saving, UserDesignated Session

--- mainwindow.h
+++ mainwindow.h
@@ -13,6 +13,7 @@
 #define GROUP_COUNT 3
 #define SESSION_TYPE_COUNT 8
 #define MAX_INTENSITY 8
+#define MAX_SESSION_LENGTH 180
 
 
 QT_BEGIN_NAMESPACE
@@ -32,12 +33,17 @@ private:
     QElapsedTimer buttonTimer;
     bool powerStatus;
 
+    bool recordSession;
+
     QVector <QLabel*> barVector;
     QVector <QLabel*> groupVector;
     QVector <QLabel*> sessionVector;
 
+    //todo: Vector of sessions
+    QVector <Session*> sessionHistory;
+
+
     int currentGroup;
-    int currentGroupDuration;
     int currentSessionType;
 
     int currentIntensity;
@@ -49,9 +55,11 @@ private:
 
     //Power indicator scene
     QGraphicsScene *pi_scene;
-    QGraphicsScene *n1_scene;
+    //Record indicator scene
+    QGraphicsScene *rec_scene;
 
     float battery;
+    double userSessionTime;
 
     Session* currentSession;
 
@@ -82,8 +90,11 @@ private slots:
     void togglePowerButton();
     void toggleIntensityUp();
     void toggleIntensityDown();
+    void toggleDurationUp();
+    void toggleDurationDown();
     void startSession();
     void changeConnection();
+    void toggleRecording();
 
 };
 #endif // MAINWINDOW_H
